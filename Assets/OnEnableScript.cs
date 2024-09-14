75308193df6213b825117e26f19a751b1c150039
Josef <66090566+Josefhz@users.noreply.github.com>
2024-09-14 17:02:06 -0300

    commit
    NavMesh + Player + Organizacao

---
+++ Assets/OnEnableScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnableScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("AWAKE CALLED");
+    }
+
+    void Start()
+    {
+        Debug.Log("START CALLED");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("ON ENABLE CALLED " + Random.Range(0,99999));
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("ON DISABLE CALLED " + Random.Range(0, 99999));
+    }
+
+    private void Update()
+    {
+        Debug.Log("UPDATE CALLED");
+    }
+}
---
+++ Assets/_src/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Layers")]
+    public LayerMask groundLayer;
+
+    private NavMeshAgent nav;
+
+    private void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+        {
+            if (hit.collider.CompareTag("Ground"))
+            {
+                Debug.Log("HITANDO CHAO");
+
+                nav.SetDestination(hit.point);
+            }
+        }
+    }
+}
